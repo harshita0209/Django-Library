bf7212b585a8449a9cebb56a66c4d16bf7d29ae8
Harshita <hnagpal0209@gmail.com>
2021-06-21 22:41:21 +0530

    Django Library App

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Django-Library
 The Local Library website
+
+Welcome to the "Local Library" Django App,that might be used to manage the catalog for a local library. 
+
+In this Project I have used:
+
+Django's tools to create a skeleton website and application.
+Creating a skeleton website
+Start and stop the development server
+Using models
+Django admin site
+Creating our home page
+Generic list and detail views
+Sessions framework
+User authentication and permissions
---
+++ locallibrary/catalog/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+
+# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+# class BookListView(generic.ListView):
+#     model = Book
+
+#     def get_queryset(self):
+#         return Book.objects.filter(title__icontains='war')[:5]
+
+# class BookListView(generic.ListView):
+#     model = Book
+
+#     def get_context_data(self, **kwargs):
+#         # Call the base implementation first to get the context
+#         context = super(BookListView, self).get_context_data(**kwargs)
+#         # Create any data and add it to the context
+#         context['some_data'] = 'This is just some data'
+#         return context
+
+    
+from django.shortcuts import get_object_or_404
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 3
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
